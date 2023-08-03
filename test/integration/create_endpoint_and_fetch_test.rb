84905bdc3d7922df80d36434e24ba93c115ab01b
Yogen Aggarwal <yogenaggarwal@Yogens-MacBook-Air.local>
2023-08-03 07:00:57 +0530

    Add happy flow integration test

---
+++ test/integration/create_endpoint_and_fetch_test.rb
@@ -0,0 +1,21 @@
+require "test_helper"
+
+class CreateEndpointAndFetchTest < ActionDispatch::IntegrationTest
+  test "should create endpoint and fetch" do
+    @endpoint = build(:valid_get_endpoint)
+    post endpoints_url, params: {data: {type: "endpoints", attributes: { path: @endpoint.path, response: @endpoint.response, verb: @endpoint.verb }}}, as: :json, headers: {
+      'Accept' => JSONAPI::MEDIA_TYPE,
+      'Content-Type' => JSONAPI::MEDIA_TYPE
+    }
+    get_response_data = get @endpoint.path, as: :json, headers: {
+      'Accept' => JSONAPI::MEDIA_TYPE,
+      'Content-Type' => JSONAPI::MEDIA_TYPE
+    }
+    post_response_data = post @endpoint.path, as: :json, headers: {
+      'Accept' => JSONAPI::MEDIA_TYPE,
+      'Content-Type' => JSONAPI::MEDIA_TYPE
+    }
+    assert_equal 200, get_response_data
+    assert_equal 404, post_response_data
+  end
+end
